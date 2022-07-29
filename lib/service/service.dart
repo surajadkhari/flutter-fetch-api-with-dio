8f47e47c5b0265c122edd616a75b5babc7842bc4
unknown <surajadkhari@gmail.com>
2022-07-29 18:45:08 +0545

    create dioclient-getpost() method

---
+++ lib/service/service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_fetch_api_with_dio/model/post_model.dart';
+
+class ApiService {
+  final String endpoint = "https://jsonplaceholder.typicode.com/";
+  final String path = "posts";
+  Future<List<PostModel>> getPost() async {
+    try {
+      final response = await Dio(BaseOptions(baseUrl: endpoint)).get(path);
+      final List data = response.data;
+      return data.map((e) => PostModel.fromJson(e)).toList();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
