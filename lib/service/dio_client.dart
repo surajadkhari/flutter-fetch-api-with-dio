2990b12eef9dc061dae8bbd6a14659f8f9547042
unknown <surajadkhari@gmail.com>
2022-07-31 14:21:55 +0545

    refactor const

--- lib/service/dio_client.dart
+++ lib/service/dio_client.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_fetch_api_with_dio/model/user_model.dart';
+import 'package:flutter_fetch_api_with_dio/utlis/const.dart';
 
 class DioClient {
-  final String endpoint = "https://reqres.in/api/";
-  final String path = "users?page=2";
   Future<List<UserModel>> getPost() async {
     try {
-      final response = await Dio(BaseOptions(baseUrl: endpoint)).get(path);
+      final response = await Dio(BaseOptions(baseUrl: ApiConst.endpoint))
+          .get(ApiConst.endpoint);
       final List data = response.data["data"];
       return data.map((e) => UserModel.fromJson(e)).toList();
     } catch (e) {
---
+++ lib/utlis/const.dart
@@ -0,0 +1,5 @@
+class ApiConst {
+  ApiConst._();
+  static const String endpoint = "https://reqres.in/api/";
+  static const String path = "users?page=2";
+}
