ee68685867b8d9be81cb003ac0f8440299439723
unknown <surajadkhari@gmail.com>
2022-07-31 13:56:41 +0545

    change endpoint

--- lib/service/dio_client.dart
+++ lib/service/dio_client.dart
@@ -2,13 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:flutter_fetch_api_with_dio/model/user_model.dart';
 
 class DioClient {
-  final String endpoint = "https://jsonplaceholder.typicode.com/";
-  final String path = "posts";
-  Future<List<PostModel>> getPost() async {
+  final String endpoint = "https://reqres.in/api/";
+  final String path = "users?page=2";
+  Future<List<UserModel>> getPost() async {
     try {
       final response = await Dio(BaseOptions(baseUrl: endpoint)).get(path);
       final List data = response.data;
-      return data.map((e) => PostModel.fromJson(e)).toList();
+      return data.map((e) => UserModel.fromJson(e)).toList();
     } catch (e) {
       throw Exception(e);
     }
